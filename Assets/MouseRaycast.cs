fbe385091dbcff1ae98ce8e9eaf68a5e1d790c96
rlawkdgn123 <rlawkdgn123@naver.com>
2025-02-12 21:32:22 +0900

    드래그 버그 1차 수정

--- Assets/MouseRaycast.cs
+++ Assets/MouseRaycast.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MouseRaycast : MonoBehaviour
 {
     public LayerMask layer;
+    private GameObject obj;
     void Update()
     {
         // 마우스 화면 좌표를 월드 공간의 Ray로 변환
@@ -19,6 +20,16 @@ public class MouseRaycast : MonoBehaviour
         {
             // 오브젝트가 마우스 위치에 접촉했을 때
             Debug.Log("오브젝트에 접촉됨: " + hit.collider.name);
+            if (Input.GetMouseButton(0))
+            {
+                obj = hit.transform.gameObject;
+                obj.transform.position = new Vector3(hit.point.x, hit.point.y, 0);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                obj = null;
+            }
+            
 
             // hit.point는 마우스 위치에서 Ray가 닿은 WorldSpace 좌표입니다.
             Debug.Log("x좌표 : "+hit.point.x +"y좌표 : "+hit.point.y);
