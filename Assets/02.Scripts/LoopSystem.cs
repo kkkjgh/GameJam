7b70a27ecadabab2ae1b65895e254b27a729e843
rlawkdgn123 <rlawkdgn123@naver.com>
2025-02-13 21:25:50 +0900

    루프시스템 수정 및 버튼 수정
    튜토리얼 - 타이틀 - 메인 이동 버튼 로직 수정 및 개선

--- Assets/02.Scripts/LoopSystem.cs
+++ Assets/02.Scripts/LoopSystem.cs
@@ -12,33 +12,34 @@ public class LoopSystem : MonoBehaviour
 
     void Awake()
     {
-        if (null == instance)
+        // 이미 인스턴스가 존재하지 않으면
+        if (instance == null)
         {
-            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+            // 인스턴스를 자신으로 설정
             instance = this;
 
+            // 씬 전환 시 이 객체가 파괴되지 않도록 설정
             DontDestroyOnLoad(this.gameObject);
         }
         else
         {
+            // 이미 인스턴스가 존재하면 이 객체를 파괴
             Destroy(this.gameObject);
         }
     }
+
     public static LoopSystem Instance
     {
         get
         {
-            if (null == instance)
-            {
-                return null;
-            }
+            // 인스턴스가 null이면 null을 반환
             return instance;
         }
     }
 
     public bool wrongCheck = false;
     public int MaxStage = 4;
-    public int stage = 0;
+    public int stage = -1;
     public Text stageText;
     public int wrongCount = 0;
     public int GetStage()
@@ -53,13 +54,32 @@ public class LoopSystem : MonoBehaviour
         }
         stage++;
         string sceneName = "Main" + stage;
-        SceneManager.LoadScene(String.Format(sceneName));
+        SceneManager.LoadScene(sceneName);
         
     }
     public void GoToScene(int scene)
     {
-        stage = scene; 
-        SceneManager.LoadScene(String.Format(scene.ToString()));
+        stage = scene;
+        if (scene == -1)
+        {
+            stage = -1;
+            SceneManager.LoadScene("Title");
+        }
+        else if (scene == 0)
+        {
+            SceneManager.LoadScene("Tuto");
+            stage = 0;
+        }
+        else if (scene < MaxStage && scene > 0)
+        {
+            SceneManager.LoadScene("Main"+scene);
+            stage = scene;
+        }
+        else if (scene == MaxStage - 1)
+        {
+            SceneManager.LoadScene("Finish");
+            stage = MaxStage - 1;
+        }
     }
     public void GoToStart()
     {
