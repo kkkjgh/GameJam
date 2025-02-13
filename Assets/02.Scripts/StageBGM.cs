79e9bc7a0bcf2a2cf58d1c6f89c176649852f88c
DanJi-NaRi <bjh000523@naver.com>
2025-02-13 13:57:47 +0900

    타이틀 및 스테이지 브금 추가
    Titlecut 스크립트에 버튼 현재 브금 정지 기능 추가

--- Assets/02.Scripts/StageBGM.cs
+++ Assets/02.Scripts/StageBGM.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleBGM: MonoBehaviour
+public class StageBGM : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
 
-
-        SoundManager.Instance.playBGM(SoundManager.BGM.BGM_TITLE); // 타이틀 브금
-        //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_STAGE); // 스테이지 브금
+        //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_TITLE); // 타이틀 브금
+        SoundManager.Instance.playBGM(SoundManager.BGM.BGM_STAGE); // 스테이지 브금
         //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_FINISH); // 엔딩 브금
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
