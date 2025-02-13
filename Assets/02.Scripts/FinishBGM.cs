90283e1a0ed8b4381283e354a22ff70b550f810b
DanJi-NaRi <bjh000523@naver.com>
2025-02-14 04:06:35 +0900

    피니시 브그

--- Assets/02.Scripts/FinishBGM.cs
+++ Assets/02.Scripts/FinishBGM.cs
@@ -14,9 +14,9 @@ public class FimishBGM : MonoBehaviour
         
 
 
-        SoundManager.Instance.playBGM(SoundManager.BGM.BGM_TITLE); // 타이틀 브금
+        //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_TITLE); // 타이틀 브금
         //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_STAGE); // 스테이지 브금
-        //SoundManager.Instance.playBGM(SoundManager.BGM.BGM_FINISH); // 엔딩 브금
+        SoundManager.Instance.playBGM(SoundManager.BGM.BGM_FINISH); // 엔딩 브금
     }
 
     // Update is called once per frame
