9b87581d3b7fc66cf444b2c2f9d71b5ec70e89f3
DanJi-NaRi <bjh000523@naver.com>
2025-02-14 03:01:11 +0900

    중간 커밋

--- Assets/02.Scripts/ButtonManager.cs
+++ Assets/02.Scripts/ButtonManager.cs
@@ -5,6 +5,7 @@ public class ButtonManager : MonoBehaviour
 {
     public Button nextButton; // NextButton (캔버스에 있는 버튼을 연결)
     public int buttonClickCount = 0; // 클릭된 버튼 수
+    public bool flag;
 
     void Start()
     {
@@ -12,15 +13,20 @@ public class ButtonManager : MonoBehaviour
       //  nextButton.interactable = false;
     }
 
+    void Update()
+    {
+        if (buttonClickCount >= 5)
+        {
+            flag = true;
+            //  nextButton.interactable = true; // NextButton 활성화
+        }
+    }
     // 버튼이 클릭될 때 호출되는 함수
     public void OnButtonClicked()
     {
         buttonClickCount++; // 버튼 클릭 수 증가
 
         // 5개 버튼을 모두 클릭한 경우 NextButton을 활성화
-        if (buttonClickCount >= 5)
-        {
-          //  nextButton.interactable = true; // NextButton 활성화
-        }
+        
     }
 }
