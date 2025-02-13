3e4975e7a2bd19bc7c55672af7674d89d618a758
DanJi-NaRi <bjh000523@naver.com>
2025-02-14 04:14:45 +0900

    플래그

--- Assets/02.Scripts/NextButton.cs
+++ Assets/02.Scripts/NextButton.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class NextButton : MonoBehaviour
 {
-    private ButtonManager buttonManager; // ButtonManager를 참조할 변수
 
     public bool flag;
     public void next()
     {
-        buttonManager = FindObjectOfType<ButtonManager>();
-        flag = buttonManager.flag;
         if (flag)
         {
             LoopSystem.Instance.GoToNextScene();
         }
     }
 }
-
