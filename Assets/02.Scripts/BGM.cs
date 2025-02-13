80efd50a7113e5eecc284e0108a94bf4588f59af
DanJi-NaRi <bjh000523@naver.com>
2025-02-13 20:25:08 +0900

    사운드 최종 커밋

--- Assets/02.Scripts/BGM.cs
+++ Assets/02.Scripts/BGM.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class BGM : MonoBehaviour
 {
-    public SoundManager.BGM backgroundMusic;  //  BGM 종류 (Inspector에서 설정 가능)
+    public SoundManager.BGM backgroundMusic;  // BGM 종류 (Inspector에서 설정 가능)
+    public float volume = 1f;  // BGM 볼륨 (기본값: 1)
 
-    // BGM 재생
+    // BGM 재생 (볼륨도 함께 받는 형태로 수정)
     public void PlayBGM()
     {
         SoundManager.Instance.playBGM(backgroundMusic);  // 선택한 BGM 재생
+        SoundManager.Instance.SetBGMVolume(volume);  // 볼륨 설정
     }
 
     // BGM 멈추기
@@ -16,7 +18,7 @@ public class BGM : MonoBehaviour
         SoundManager.Instance.stopBGM();  // BGM 멈추기
     }
 
-    // BGM을 서서히 끄는 기능 추가 (FadeOutBGM)
+    // BGM을 서서히 끄는 기능 (FadeOutBGM)
     public void FadeOutBGM(float fadeDuration)
     {
         SoundManager.Instance.FadeOutBGM(fadeDuration);  // FadeOutBGM 호출
