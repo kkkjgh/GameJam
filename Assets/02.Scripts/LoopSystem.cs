835e5213c357e910f50276b55775e31830a8ee2d
rlawkdgn123 <rlawkdgn123@naver.com>
2025-02-14 05:59:27 +0900

    피니쉬 재업로드

--- Assets/02.Scripts/LoopSystem.cs
+++ Assets/02.Scripts/LoopSystem.cs
@@ -49,7 +49,7 @@ public class LoopSystem : MonoBehaviour
     public void GoToNextScene()
     {
         stage++;
-        if ((stage + 1) == MaxStage)
+        if (stage <= 5)
         {
             SceneManager.LoadScene("Finish");
         }if (stage == 0)
@@ -84,7 +84,7 @@ public class LoopSystem : MonoBehaviour
             
             stage = scene;
         }
-        else if (scene == MaxStage - 1)
+        else if (scene <= 5)
         {
             SceneManager.LoadScene("Finish");
             stage = MaxStage - 1;
